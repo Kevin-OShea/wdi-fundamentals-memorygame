70cb02b23fe0b376222ec92c2f0326a843e7bdf4
breakfastorder <kevinbarretoshea@gmail.com>
2020-01-08 12:35:20 -0500

    created game logic

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -1,7 +1,18 @@
 console.log("up and running!");
-let cardOne = "queen";
-let cardTwo = "queen";
-let cardThree = "king";
-let cardFour = "king";
 
-console.log("User Flipped " + cardOne + " and " + cardTwo);
+var cards = ["queen","queen","king","king"];
+var cardsInPlay = [];
+
+let cardOne = cards[0];
+let cardTwo = cards[2];
+cardsInPlay.push(cardOne);
+cardsInPlay.push(cardTwo);
+console.log("User Flipped queen");
+console.log("User Flipped king");
+
+if(cardsInPlay[0] === cardsInPlay[1])
+{
+  alert("You Found a Match");
+}else{
+  alert("Sorry, try again");
+}
