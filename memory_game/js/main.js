b16b0026eb4d44e6b16e41cbfa856d87230c496d
breakfastorder <kevinbarretoshea@gmail.com>
2020-01-09 16:14:42 -0500

    added game functions

--- memory_game/js/main.js
+++ memory_game/js/main.js
@@ -23,6 +23,8 @@ const cards = [
   }
 
 ];
+
+let counter = 0;
 var cardsInPlay = [];
 
 function checkForMatch()
@@ -30,20 +32,25 @@ function checkForMatch()
   if(cardsInPlay[0] === cardsInPlay[1])
   {
     console.log("You found a match!");
+    counter++;
+    //alert("You found a match!");
   }else{
     console.log("Sorry, try again.");
+    counter--;
+    //alert("Sorry, try again.");
   }
 }
 
 
-function flipCard(cardId)
+function flipCard()
 {
+  let cardId = this.getAttribute('data-id');
   console.log("User flipped " + cards[cardId].rank);
   cardsInPlay.push(cards[cardId].rank);
-  console.log(cards[cardId].cardImage);
-  console.log(cards[cardId].suit);
-
-  if(cardsInPlay.length === 2)
+  //console.log(cards[cardId].cardImage);
+  //console.log(cards[cardId].suit);
+  this.setAttribute('src',cards[cardId].cardImage);
+  if(cardsInPlay.length === 2)//fixing logic where it would create false negatives due to checking against null
   {
     if (cardsInPlay[0] === cardsInPlay[1]) {
         checkForMatch()
@@ -51,8 +58,43 @@ function flipCard(cardId)
         checkForMatch()
     }
   }
+  console.log("Score: " + counter);
+}
+
+function createBoard()
+{
+  for (let i = 0; i < cards.length; i++) {
+    let cardElement = document.createElement('img');
+    cardElement.setAttribute('src',"images/back.png");
+    cardElement.setAttribute('data-id',i);
+    cardElement.addEventListener('click',flipCard);
+    document.getElementById('game-board').appendChild(cardElement);
+  }
+
 
 }
 
-flipCard(0);
-flipCard(2);
+function createButton()
+{
+  let resetButton = document.getElementById('reset');
+  resetButton.addEventListener('click',reset);
+}
+
+function reset()
+{
+  //go through cards played and reset images
+  let list = document.getElementById('game-board');
+  while(list.firstChild) {
+    list.removeChild(list.firstChild);
+  }
+  createBoard();
+  //turns out that game-board is holding the cards so I actually dont touch cardsInPlay
+  //reset the cardsInPlay array
+  cardsInPlay = [];
+
+  console.log(cardsInPlay.length);
+}
+
+
+createBoard();
+createButton();
